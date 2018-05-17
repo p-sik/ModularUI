5b3e49e4785f9467ff3f976a8a6e77dd06d32c6d
Peter-stac <p3ter.sik@gmail.com>
2018-05-17 15:12:25 +0200

    Refactored FAB to inherit from InteractableGraphic

---
+++ Assets/UIBase/GraphicElements/BaseClasses/InteractableGraphic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.UIBase.GraphicElements.BaseClasses
+{
+    [RequireComponent(typeof(Button))]
+    [RequireComponent(typeof(Image))]
+    public class InteractableGraphic : ModularUI
+    {
+        protected Image buttonImage;
+        protected Button button;
+        protected RectTransform buttonRectTransform;
+
+        protected override void OnSkinUI()
+        {
+            base.OnSkinUI();
+
+            RetreiveComponents();
+            SetupButton();
+            SetupButtonImage();
+            DisableAutomaticNavigation();
+        }
+
+        protected void RetreiveComponents()
+        {
+            buttonImage = GetComponent<Image>();
+            button = GetComponent<Button>();
+            buttonRectTransform = GetComponent<RectTransform>();
+        }
+
+        protected virtual void SetupButton()
+        {
+            button.transition = Selectable.Transition.SpriteSwap;
+            button.targetGraphic = buttonImage;
+            button.spriteState = skinData.FloatingButtonSpriteState;         
+
+            buttonRectTransform.pivot = Vector2.one * 0.5f;
+        }
+
+        protected void SetupButtonImage()
+        {
+            buttonImage.type = Image.Type.Sliced;
+            buttonImage.sprite = skinData.FloatingButtonSprite;
+        }
+
+        protected void DisableAutomaticNavigation()
+        {
+            Navigation navigationDisabler = new Navigation
+            {
+                mode = Navigation.Mode.None
+            };
+            button.navigation = navigationDisabler;
+        }
+    }
+}
--- Assets/UIBase/GraphicElements/FloatingUIButton.cs
+++ Assets/UIBase/GraphicElements/FloatingUIButton.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Assets.UIBase.GraphicElements.BaseClasses;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,59 +7,17 @@ using UnityEngine.UI;
 /// <summary>
 /// A floating action button that grows when clicked
 /// </summary>
-[RequireComponent(typeof(Button))]
-[RequireComponent(typeof(Image))]
-public class FloatingUIButton : ModularUI
+public class FloatingUIButton : InteractableGraphic
 {
     const float PRESSED_SIZE = 400f;
     const float NORMAL_SIZE = 160f;
 
     bool isPressed = false;
 
-    Image buttonImage;
-    Button button;
-    RectTransform buttonRectTransform;
-
-    protected override void OnSkinUI()
+    protected override void SetupButton()
     {
-        base.OnSkinUI();
-        
-        RetreiveComponents();
-        SetupButton();
-        SetupButtonImage();
-        DisableAutomaticNavigation();
-    }
-
-    private void RetreiveComponents()
-    {
-        buttonImage = GetComponent<Image>();
-        button = GetComponent<Button>();
-        buttonRectTransform = GetComponent<RectTransform>();
-    }
-
-    private void SetupButton()
-    {
-        button.transition = Selectable.Transition.SpriteSwap;
-        button.targetGraphic = buttonImage;
-        button.spriteState = skinData.FloatingButtonSpriteState;
+        base.SetupButton();
         button.onClick.AddListener(ChangeSize);
-
-        buttonRectTransform.pivot = Vector2.one * 0.5f;
-    }
-
-    private void SetupButtonImage()
-    {
-        buttonImage.type = Image.Type.Sliced;
-        buttonImage.sprite = skinData.FloatingButtonSprite;
-    }
-
-    private void DisableAutomaticNavigation()
-    {
-        Navigation navigationDisabler = new Navigation
-        {
-            mode = Navigation.Mode.None
-        };
-        button.navigation = navigationDisabler;
     }
 
     private void ChangeSize()
