73c5f567ef5508563655c8834329ab681194cabf
Peter-stac <p3ter.sik@gmail.com>
2018-05-18 18:44:48 +0200

    Created pop-up panel class and asset in the Editor

---
+++ Assets/UIBase/GraphicElements/PopUpPanel.cs
@@ -0,0 +1,45 @@
+﻿using Assets.UIBase.GraphicElements.BaseClasses;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// TODO flesh out ideas
+/// how to pop out
+/// how will it appear
+/// where will it be positioned
+/// </summary>
+public class PopUpPanel : ModularPanel, IMovablePanel
+{
+    public Vector2 InitialPosition
+    {
+        get { return Vector2.zero; }
+        set { }
+    }
+
+    public Vector2 FinalPosition
+    {
+        get { return Vector2.zero; }
+        set { }
+    }
+
+    protected override void OnSkinUI()
+    {
+        base.OnSkinUI();
+    }
+
+    public void Appear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Disappear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ShowOrHideDrawer()
+    {
+        throw new System.NotImplementedException();
+    }
+}
