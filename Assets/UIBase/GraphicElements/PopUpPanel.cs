69010e7dbe9ad4f320437f223bd4ec3e699e7802
Peter-stac <p3ter.sik@gmail.com>
2018-05-21 09:24:22 +0200

    Pop up panel works, but is a mess

--- Assets/UIBase/GraphicElements/PopUpPanel.cs
+++ Assets/UIBase/GraphicElements/PopUpPanel.cs
@@ -3,43 +3,151 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// TODO flesh out ideas
-/// how to pop out
-/// how will it appear
-/// where will it be positioned
-/// </summary>
 public class PopUpPanel : ModularPanel, IMovablePanel
 {
+    [SerializeField] Transform objectToHideBehind;
+    [SerializeField] Vector2 fallBackPosition;
+    bool isExtended = false;
+
     public Vector2 InitialPosition
     {
-        get { return Vector2.zero; }
+        get
+        {
+            Vector2 positionToReturn;
+            try
+            {
+                positionToReturn = objectToHideBehind.position;
+            }
+            catch (UnassignedReferenceException e)
+            {
+                Debug.LogWarning(e.Message);
+                positionToReturn = fallBackPosition;
+            }
+            return positionToReturn;
+        }
         set { }
     }
 
     public Vector2 FinalPosition
     {
-        get { return Vector2.zero; }
+        get { return InitialPosition; }
         set { }
     }
 
     protected override void OnSkinUI()
     {
         base.OnSkinUI();
+
+        panelRectTransform.localPosition = InitialPosition;
+        SetPanelSize(0, 0);
+    }
+
+    private void SetPanelSize(float xSize, float ySize)
+    {
+        panelRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, xSize);
+        panelRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, ySize);
     }
 
     public void Appear()
     {
-        throw new System.NotImplementedException();
+        StartCoroutine(IncreaseSizeGradually(true));
     }
 
     public void Disappear()
     {
-        throw new System.NotImplementedException();
+        StartCoroutine(IncreaseSizeGradually(false));
     }
 
     public void ShowOrHideDrawer()
     {
-        throw new System.NotImplementedException();
+        if (!isExtended)
+        {
+            Appear();
+            isExtended = true;
+        }
+        else
+        {
+            Disappear();
+            isExtended = false;
+        }
+    }
+
+    //IMPROVE make it not suck so much, refactor completely
+    private IEnumerator IncreaseSizeGradually(bool isExtending)
+    {
+        float currentXDimension = panelRectTransform.rect.width;
+        float currentYDimension = panelRectTransform.rect.height;
+        bool isXLarger = panelDimensions.XDimension > panelDimensions.YDimension;
+
+        if (isExtending)
+        {
+            if (isXLarger)
+            {
+                while (currentXDimension < panelDimensions.XDimension)
+                {
+                    if (currentYDimension < panelDimensions.YDimension)
+                    {
+                        currentYDimension += 20;
+                    }
+
+                    currentXDimension += 20;
+
+                    SetPanelSize(currentXDimension, currentYDimension);
+
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+            else
+            {
+                while (currentYDimension < panelDimensions.YDimension)
+                {
+                    if (currentXDimension < panelDimensions.XDimension)
+                    {
+                        currentXDimension += 20;
+                    }
+
+                    currentYDimension += 20;
+
+                    SetPanelSize(currentXDimension, currentYDimension);
+
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
+        else
+        {
+            if (isXLarger)
+            {
+                while (currentXDimension > 0)
+                {
+                    if (currentYDimension > 0)
+                    {
+                        currentYDimension -= 20;
+                    }
+
+                    currentXDimension -= 20;
+
+                    SetPanelSize(currentXDimension, currentYDimension);
+
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+            else
+            {
+                while (currentYDimension > 0)
+                {
+                    if (currentXDimension > 0)
+                    {
+                        currentXDimension -= 20;
+                    }
+
+                    currentYDimension -= 20;
+
+                    SetPanelSize(currentXDimension, currentYDimension);
+
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
     }
 }
