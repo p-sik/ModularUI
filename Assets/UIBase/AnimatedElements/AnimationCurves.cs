bd929650b1fc7a0e0ee8b611f285ff68439b50c0
Peter-stac <p3ter.sik@gmail.com>
2018-05-22 11:12:44 +0200

    created curve class

---
+++ Assets/UIBase/AnimatedElements/AnimationCurves.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.UIBase.AnimatedElements
+{
+    public class AnimationCurves
+    {
+        //TODO implement different animated curves to make them more appealing
+    }
+}
